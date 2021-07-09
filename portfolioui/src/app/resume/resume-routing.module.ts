e21a68232c50e689084f44890bc7cfb577487cf5
advaithsuryaP <padvaithsurya@gmail.com>
2021-07-10 01:17:05 +0530

    Designing home component

--- portfolioui/src/app/resume/resume-routing.module.ts
+++ portfolioui/src/app/resume/resume-routing.module.ts
@@ -4,12 +4,14 @@ import { AboutComponent } from './resume/about/about.component';
 import { CertificationsComponent } from './resume/certifications/certifications.component';
 import { EducationComponent } from './resume/education/education.component';
 import { ExperienceComponent } from './resume/experience/experience.component';
+import { HomeComponent } from './resume/home/home.component';
 import { ProjectsComponent } from './resume/projects/projects.component';
 import { ResumeComponent } from './resume/resume.component';
 import { SkillsComponent } from './resume/skills/skills.component';
 
 const routes: Routes = [
   { path: '', component: ResumeComponent, children: [
+    { path: '', pathMatch: 'full', component: HomeComponent },
     { path: 'about', component: AboutComponent },
     { path: 'education', component: EducationComponent },
     { path: 'experience', component: ExperienceComponent },
--- portfolioui/src/app/resume/resume.module.ts
+++ portfolioui/src/app/resume/resume.module.ts
@@ -2,19 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ResumeRoutingModule } from './resume-routing.module';
-import { ResumeComponent } from './resume/resume.component';
-import { ResumeHeaderComponent } from './resume-header/resume-header.component';
 
-import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import {} from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
+import { ResumeComponent } from './resume/resume.component';
+import { HomeComponent } from './resume/home/home.component';
+import { AboutComponent } from './resume/about/about.component';
+import { SkillsComponent } from './resume/skills/skills.component';
+import { ProjectsComponent } from './resume/projects/projects.component';
 import { EducationComponent } from './resume/education/education.component';
 import { ExperienceComponent } from './resume/experience/experience.component';
-import { SkillsComponent } from './resume/skills/skills.component';
+import { ResumeHeaderComponent } from './resume-header/resume-header.component';
 import { CertificationsComponent } from './resume/certifications/certifications.component';
-import { ProjectsComponent } from './resume/projects/projects.component';
-import { AboutComponent } from './resume/about/about.component';
 
 
 @NgModule({
@@ -26,13 +27,15 @@ import { AboutComponent } from './resume/about/about.component';
     SkillsComponent,
     CertificationsComponent,
     ProjectsComponent,
-    AboutComponent
+    AboutComponent,
+    HomeComponent
   ],
   imports: [
     CommonModule,
     ResumeRoutingModule,
     MatButtonModule,
     MatIconModule,
+    MatCardModule,
   ],
   exports: [
     MatButtonModule,
