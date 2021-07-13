07b934bebaa7f3ce4f1f02d9d360107317581e40
advaithsuryaP <padvaithsurya@gmail.com>
2021-07-13 14:24:50 +0530

    Modified Raahf components import

--- portfolioui/src/app/raahf/raahf.module.ts
+++ portfolioui/src/app/raahf/raahf.module.ts
@@ -5,14 +5,15 @@ import { MaterialModule } from '../material/material.module';
 import { RaahfRoutingModule } from './raahf-routing.module';
 
 import { RaahfComponent } from './raahf/raahf.component';
+import { RaahfHomeComponent } from './raahf/raahf-home/raahf-home.component';
+import { RaahfGoalsComponent } from './raahf/raahf-goals/raahf-goals.component';
 import { RaahfSidebarComponent } from './raahf-sidebar/raahf-sidebar.component';
-import { RaahfHomeComponent } from './raahf/raahf/raahf-home/raahf-home.component';
-import { RaahfGoalsComponent } from './raahf/raahf/raahf-goals/raahf-goals.component';
-import { RaahfHealthComponent } from './raahf/raahf/raahf-health/raahf-health.component';
-import { RaahfFinanceComponent } from './raahf/raahf/raahf-finance/raahf-finance.component';
-import { RaahfSecurityComponent } from './raahf/raahf/raahf-security/raahf-security.component';
-import { RaahfLearningComponent } from './raahf/raahf/raahf-learning/raahf-learning.component';
-import { RaahfDashboardComponent } from './raahf/raahf/raahf-dashboard/raahf-dashboard.component';
+import { RaahfFinanceComponent } from './raahf/raahf-finance/raahf-finance.component';
+import { RaahfHealthComponent } from './raahf/raahf-health/raahf-health.component';
+import { RaahfLearningComponent } from './raahf/raahf-learning/raahf-learning.component';
+import { RaahfSecurityComponent } from './raahf/raahf-security/raahf-security.component';
+import { RaahfDashboardComponent } from './raahf/raahf-dashboard/raahf-dashboard.component';
+
 
 
 @NgModule({
